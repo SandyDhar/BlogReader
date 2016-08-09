f0f74e818f862105d233bc81e406ca4b199d0aa1
SandyDhar <sudeepthiosdev@gmail.com>
2016-08-09 10:40:44 -0400

    implemented google API

---
+++ BlogReader/DetailViewController.swift
@@ -0,0 +1,46 @@
+//
+//  DetailViewController.swift
+//  BlogReader
+//
+//  Created by Sudeepth Dharavasthu on 7/22/16.
+//  Copyright © 2016 Sudeepth Dharavasthu. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var webView: UIWebView!
+
+
+
+    var detailItem: AnyObject? {
+        didSet {
+            // Update the view.
+            self.configureView()
+        }
+    }
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = self.detailItem{
+        if let postWebView = self.webView {
+            postWebView.loadHTMLString(detail.valueForKey("content")!.description, baseURL: nil)
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.configureView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
